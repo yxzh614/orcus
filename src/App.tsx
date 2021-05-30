462abfd43eeeb6a1a337836d9f906bc7eeaa778d
yxzh614 <526686022@qq.com>
2021-05-30 23:56:46 +0800

    feat: 控制反转

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,75 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { bindCallback, Observable, Subject } from 'rxjs';
+import { setInterval, setTimeout } from 'timers';
+import Form, { Field } from 'rc-field-form';
 
-import { HomePage } from './pages/home';
-import { SelfChangePage } from './pages/selfChange';
-import { StockSearchPage } from './pages/stockSearch';
-import { LoopDepPage } from './pages/loopDep';
+const observable = new Observable<string>(subscriber => {
+  subscriber.next('1');
+  subscriber.next('2');
+  subscriber.next('3');
+  setTimeout(() => {
+    subscriber.next('4');
+    subscriber.complete();
+  }, 1000);
+});
+
+const formJson = {
+  type: 'input'
+}
+/**
+ * 
+ * stream
+ * 
+ * userInput
+ * newChangedFields
+ * getPrice
+ * getPolicy
+ * getStock
+ * 
+ */
+
+const useGetPrice = (getPriceClicked: boolean, callback: (number: number) => void) => {
+  setInterval(() => {
+    if (!getPriceClicked) {
+      return;
+    }
+    callback(Math.random())
+  }, 1000)
+  return () => {
+    callback(Math.random())
+  }
+}
+
 
 function App() {
+  const handleFormChange = (formValue: any, callback: (formValue: any) => void) => {
+    callback(formValue)
+  }
+  const $callback = bindCallback(handleFormChange)
+  const handleValue = (v: any) => {
+    console.log(v)
+  }
+  const [getPriceClicked, setGetPriceClicked] = useState(false);
+  const getPrice = useGetPrice(getPriceClicked, (newPrice) => { // 控制剥离，hook只关心业务逻辑，不关心渲染
+    if (newPrice > 0.5) {
+      console.log(newPrice)
+    }
+  })
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route exact path="/selfChange">
-            <SelfChangePage />
-          </Route>
-          <Route exact path="/stockSearch">
-            <StockSearchPage />
-          </Route>
-          <Route exact path="/loopDep">
-            <LoopDepPage />
-          </Route>
-        </Switch>
-      </Router>
+      <Form
+        onValuesChange={(v) => {
+          $callback(v).subscribe({
+            next: handleValue
+          })
+        }}
+      >
+        <Field name={'price'}>
+          <input />
+        </Field>
+        <button onClick={() => {getPrice();setGetPriceClicked(true)}} >getPrice</button>
+      </Form>
     </div>
   );
 }
